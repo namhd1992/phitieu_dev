f88d82e51e180b62001eb812106bc8460addb85d
namhd1992 <stpnghd.vn@gmail.com>
2021-07-30 08:01:49 +0700

    fix

--- src/containers/phitieu/mobile/ios/vertical/duatop.js
+++ src/containers/phitieu/mobile/ios/vertical/duatop.js
@@ -839,7 +839,7 @@ class Lucky_Rotation extends React.Component {
 									$('#ThongBao').modal('show');
 								}
 
-								this.draw(x, data.TargetX - x, y-15, data.TargetY - y)
+								this.draw(x-2, data.TargetX - x, y-12, data.TargetY - y)
 
 								setTimeout(()=>{
 									this.showScore(data.Score);
