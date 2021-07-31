c61157f3d88f7c0bcc2243a3501be7d159a44547
namhd1992 <stpnghd.vn@gmail.com>
2021-08-01 03:18:18 +0700

    fix

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -53,23 +53,13 @@ class App extends React.Component {
 						<Route exact path="/sanqua" component={SanQua_Mobile_Android} />
 						<Route exact path="/duatop" component={DuaTop_Mobile_Android} />
 					</main></div>):(<div>
-						{(horizontal)?(<div>
 						<MenuAppBar pathname={document.location.pathname} compact={this.state.compact} scrolling={this.state.scrolling}
 						data={[{ url: "home", label: "home" }, { url: "about", label: "about" }]}></MenuAppBar>
 					<main ref={(c) => this.main = c}>
 						<Route exact path="/" component={Home_Mobile} />
 						<Route exact path="/sanqua" component={SanQua_Mobile_IOS} />
 						<Route exact path="/duatop" component={DuaTop_Mobile_IOS} />
-					</main></div>):(<div>
-						<MenuAppBar pathname={document.location.pathname} compact={this.state.compact} scrolling={this.state.scrolling}
-						data={[{ url: "home", label: "home" }, { url: "about", label: "about" }]}></MenuAppBar>
-					<main ref={(c) => this.main = c}>
-						<Route exact path="/" component={Home_Mobile} />
-						<Route exact path="/sanqua" component={SanQua_Mobile_IOS_Vertical} />
-						<Route exact path="/duatop" component={DuaTop_Mobile_IOS_Vertical} />
 					</main></div>)}
-					</div>
-						 )}
 					
 				</div>):(<div>
 					<MenuAppBar pathname={document.location.pathname} compact={this.state.compact} scrolling={this.state.scrolling}
@@ -88,3 +78,46 @@ class App extends React.Component {
 
 
 export default App;
+
+
+// return (
+// 	<div style={{ backgroundColor: this.state.backgroundColor }}>
+// 		{/* <div style={{maxWidth:"1200px", margin:"auto", background: this.state.backgroundColor }}> */}
+// 		{(isMobile)?(<div>
+// 			{(isAndroid)?(<div><MenuAppBar pathname={document.location.pathname} compact={this.state.compact} scrolling={this.state.scrolling}
+// 				data={[{ url: "home", label: "home" }, { url: "about", label: "about" }]}></MenuAppBar>
+// 			<main ref={(c) => this.main = c}>
+// 				<Route exact path="/" component={Home_Mobile} />
+// 				<Route exact path="/sanqua" component={SanQua_Mobile_Android} />
+// 				<Route exact path="/duatop" component={DuaTop_Mobile_Android} />
+// 			</main></div>):(<div>
+// 				{(horizontal)?(<div>
+// 				<MenuAppBar pathname={document.location.pathname} compact={this.state.compact} scrolling={this.state.scrolling}
+// 				data={[{ url: "home", label: "home" }, { url: "about", label: "about" }]}></MenuAppBar>
+// 			<main ref={(c) => this.main = c}>
+// 				<Route exact path="/" component={Home_Mobile} />
+// 				<Route exact path="/sanqua" component={SanQua_Mobile_IOS} />
+// 				<Route exact path="/duatop" component={DuaTop_Mobile_IOS} />
+// 			</main></div>):(<div>
+// 				<MenuAppBar pathname={document.location.pathname} compact={this.state.compact} scrolling={this.state.scrolling}
+// 				data={[{ url: "home", label: "home" }, { url: "about", label: "about" }]}></MenuAppBar>
+// 			<main ref={(c) => this.main = c}>
+// 				<Route exact path="/" component={Home_Mobile} />
+// 				<Route exact path="/sanqua" component={SanQua_Mobile_IOS_Vertical} />
+// 				<Route exact path="/duatop" component={DuaTop_Mobile_IOS_Vertical} />
+// 			</main></div>)}
+// 			</div>
+// 				 )}
+			
+// 		</div>):(<div>
+// 			<MenuAppBar pathname={document.location.pathname} compact={this.state.compact} scrolling={this.state.scrolling}
+// 				data={[{ url: "home", label: "home" }, { url: "about", label: "about" }]}></MenuAppBar>
+// 			<main ref={(c) => this.main = c}>
+// 				<Route exact path="/" component={Home_Web} />
+// 				<Route exact path="/sanqua" component={SanQua_Web} />
+// 				<Route exact path="/duatop" component={DuaTop_Web} />
+// 			</main>
+// 		</div>)}
+		
+// 	</div>
+// )
