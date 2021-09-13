f47698a135ea50c0aee2f5fd1e0e8595ddb55f0a
namhd1992 <stpnghd.vn@gmail.com>
2021-09-13 16:01:01 +0700

    fix

--- src/containers/phitieu/mobile/android/sanqua.js
+++ src/containers/phitieu/mobile/android/sanqua.js
@@ -1064,7 +1064,7 @@ class Lucky_Rotation extends React.Component {
 		darthVaderImg.hide();
 		st_touch=setTimeout(()=>{
 			_this.setState({isPlay:true})
-		}, 100);
+		}, 500);
 	}
 
 	touchMove=(e)=>{
